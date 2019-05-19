fc9d5e2cfa54aa9329fc9f6ae5a05b973c3c4b11
Bret Cadle <contactme@bretcadle.com>
2019-05-19 08:52:03 -0700

    build.py now builds single file binary

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ app_name = "northwest-clock"
 version = "0_2_8"
 directory_name = app_name + "-" + version
 dist_path = "dist/" + directory_name
-work_path = "build/" + directory_name
+work_path = "/usr/bin/"
 
 print(work_path)
 
@@ -13,6 +13,7 @@ if os.path.isdir(dist_path):
     print("Version exists!")
 else:
     subprocess.Popen(['pyinstaller', 'src/clock.py',
+                      '-F',
                       '--add-data=LICENSE:.',
                       '--add-data=README.md:.',
                       '--add-data=northwest-clock.service:.',
